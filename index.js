173099982173ec3a8e9a324b7e0752b5d066ccf5
Maik Marschner <mmarschner@gdv.uni-hannover.de>
2015-08-17 16:02:58 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var config = require("cson").load("config.cson");
+var TutorServer = require("@tutor/server");
+var MemDB = require("@tutor/memory-database")(config);
+var express = require("express");
+var open = require("open")
+
+restAPI = require("./src/rest")(MemDB);
+
+config.modules = [
+  require("@tutor/dummy-auth"),
+];
+
+if(config.development){
+  config.modules.push(function(app, config){
+    app.use(express.static('./build'));
+  });
+}
+
+// create a server
+var server = TutorServer(config);
+
+// register rest API
+restAPI.forEach(function(rest){
+  server.createRestCall(rest);
+});
+
+// start the server
+server.start();
+open("http://localhost:8080");
