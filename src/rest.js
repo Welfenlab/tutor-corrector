0760cd7b8107e1a6a2ca767125effdb445b6a492
Maximilian Klein <dasmaxl@gmail.com>
2015-09-23 15:59:00 +0200

    (feature) Rest API for corrector app.

--- src/rest.js
+++ src/rest.js
@@ -1,13 +1,26 @@
 
 module.exports = function(DB){
   return [
-    { path: '/api/exercises', dataCall: DB.Exercises.getExercises, apiMethod: "get" },
-    { path: '/api/exercises/detailed/:id', dataCall: DB.Exercises.getExercise, apiMethod: "getByParam", param: "id", errStatus: 404  },
+    { path: '/api/exercises', dataCall: DB.Exercises.get, apiMethod: "get" },
+    { path: '/api/exercises/detailed/:id', dataCall: DB.Exercises.getDetailed, apiMethod: "getByParam", param: "id", errStatus: 404  },
 
-    { path: '/api/correction/pending/:id', dataCall: DB.Correction.getNumPending, apiMethod: "getByParam", param: "id", errStatus: 404  },
-    { path: '/api/correction', dataCall: DB.Correction.status, apiMethod: "get" },
-    { path: '/api/correction/next/:id', dataCall: DB.Correction.lockNextSolutionForTutor, apiMethod: "getBySessionUIDAndParam", param: "id", errStatus: 404 },
+    { path: '/api/correction/pending/:id', dataCall: DB.Corrections.getNumPending,
+      apiMethod: "getByParam", param: "id", errStatus: 404  },
+    { path: '/api/correction', dataCall: DB.Corrections.getStatus, apiMethod: "get" },
+    { path: '/api/correction/next/:id', dataCall: DB.Corrections.lockNextSolutionForTutor,
+      apiMethod: "getBySessionUIDAndParam", param: "id", errStatus: 404 },
+    { path: '/api/correction/finish', dataCall: DB.Corrections.finishSolution,
+      apiMethod: "postBySessionUIDAndParam", param: "id", errStatus: 404 },
+    { path: '/api/correction/unfinished', dataCall: DB.Corrections.getUnfinishedSolutionsForTutor,
+      apiMethod: "getBySessionUID" },
+      { path: '/api/correction/finished', dataCall: DB.Corrections.getFinishedSolutionsForTutor,
+        apiMethod: "getBySessionUID" },
+    { path: '/api/correction/store', dataCall: DB.Corrections.setResultForExercise,
+      apiMethod: "putBySessionUIDAndParams", params: ["id", "results"], errStatus: 404 },
 
-    { path: '/api/solution/:id', dataCall: DB.Exercises.getExerciseSolution, apiMethod: "getBySessionUIDAndParam", param: "id", errStatus: 404 },
+    { path: '/api/tutor', dataCall: DB.Users.getTutor, apiMethod: "getBySessionUID" },
+
+    { path: '/api/solution/:id', dataCall: DB.Exercises.getExerciseSolution,
+      apiMethod: "getBySessionUIDAndParam", param: "id", errStatus: 404 },
   ];
 };
