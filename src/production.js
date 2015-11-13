9a00d220b53e3574d9ceadeaf8610f54e1476248
Maximilian Klein <dasmaxl@gmail.com>
2015-11-13 15:48:48 +0100

    (feature) Implement production environment.

--- src/production.js
+++ src/production.js
@@ -1,19 +1,25 @@
 
+var express = require('express');
+var _ = require('lodash');
+var bcrypt = require('bcrypt-then');
 
 // initialize the production environment
-module.exports = function(app, config){
+module.exports = function(config){
   console.log("production environment");
+  config.database.host = process.env.RETHINKDB_PORT_28015_TCP_ADDR;
+  config.database.port = parseInt(process.env.RETHINKDB_PORT_28015_TCP_PORT);
   var rethinkDB = require("@tutor/rethinkdb-database")(config);
+  return rethinkDB.then(function(DB){
+    restAPI = require("./rest")(DB);
 
-  restAPI = require("./rest")(rethinkDB);
+    var tutorAuth = function(name, pw) {
+      return DB.Users.authTutor(name, _.partial(bcrypt.compare,pw));
+    };
+    config.modules = []
+    config.modules.push(require("@tutor/dummy-auth")(DB.Connection, DB.Rethinkdb, tutorAuth));
+    config.domainname = "tutor-corrector.gdv.uni-hannover.de"
 
-  var tutorAuth = function(name, pw) {
-    return MemDB.Users.authTutor(name, _.partial(bcrypt.compare,pw));
-  };
-  config.modules = []
-  config.modules.push(require("@tutor/dummy-auth")(tutorAuth));
-
-  return new Promise(function(resolve){
-    resolve(restAPI);
+    config.modules.push(function(app){ app.use(express.static('./build')); });
+    return restAPI;
   });
 }
