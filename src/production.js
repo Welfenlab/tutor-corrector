50b2c4d1cf1f8d8aa0be8201c436d071e4ed22ca
Maximilian Klein <dasmaxl@gmail.com>
2015-12-19 19:22:35 +0100

    (cleanup) Remove dummy-auth and use real auth lib.
    Also fixed some issues with the production build.

--- src/production.js
+++ src/production.js
@@ -1,17 +1,24 @@
 
+var bcrypt = require("bcrypt-then");
+var _ = require('lodash')
 
 // initialize the production environment
 module.exports = function(app, config){
   console.log("production environment");
+  console.log("... waiting 10s for RethinkDB");
+  config.database.host = process.env.RETHINKDB_PORT_28015_TCP_ADDR;
+  config.database.port = parseInt(process.env.RETHINKDB_PORT_28015_TCP_PORT);
+  config.sharejs.rethinkdb.host = config.database.host;
+  config.sharejs.rethinkdb.port = config.database.port;
   var rethinkDB = require("@tutor/rethinkdb-database")(config);
 
-  restAPI = require("./rest")(rethinkDB);
+  var restAPI = require("./rest")(rethinkDB);
 
   var tutorAuth = function(name, pw) {
-    return MemDB.Users.authTutor(name, _.partial(bcrypt.compare,pw));
+    return rethinkDB.Users.authTutor(name, _.partial(bcrypt.compare,pw));
   };
   config.modules = []
-  config.modules.push(require("@tutor/dummy-auth")(tutorAuth));
+  config.modules.push(require("@tutor/auth")(rethinkDB.Connection, rethinkDB.Rethinkdb, tutorAuth));
 
   return new Promise(function(resolve){
     resolve(restAPI);
